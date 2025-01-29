e57fab5d042e9f261f9ea4c93ebc686cca04fd1e
Moustafa <mostafamm3232@gmail.com>
2025-01-29 21:53:16 +0100

    Simple code to get a list of objects usw.

--- Main.java
+++ Main.java
@@ -1,9 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-            AverageAge name = new AverageAge();
-            name.calculateAverageAge(4);
-            System.out.println("the AverageAge age is "+ name.getAverageAge());
-        }
+        // this is HashMap
+//        HashMap<String, Object> test_json = new HashMap<>();
+//        test_json.put("Key1" , "Value1");
+//        test_json.put("Key2", "Value2");
+//        System.out.println(test_json);
+//        // this is ArrayList
+//
+//        ArrayList<String> test = new ArrayList<>();
+//        test.add("Moustafa");
+//        test.add("Ahmed");
+//        System.out.println(test);
+
+        // List
+//        HashMap<String, Object> object = new HashMap<>();
+//        HashMap<String, Object> subobject = new HashMap<>();
+//        subobject.put("work", "my work adress");
+//        subobject.put("home", "my home adress");
+//        object.put("adress", subobject);
+//        System.out.println(object);
+        List<String> myList = new ArrayList<>();
+        myList.add("Apple");
+        myList.add("Banana");
+        System.out.println(myList);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Java");
+        arrayList.add("Python");
+        System.out.println(arrayList);
+
+
     }
+}
